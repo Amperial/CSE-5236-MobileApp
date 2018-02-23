7a59198ca62dabd44e59ee6f23e2834c5228524b
Thomas Kiener <kiener.17@osu.edu>
2018-02-23 01:40:20 -0500

    Activity and Fragment logging

--- app/src/main/java/a5236/android_game/TitleFragment.java
+++ app/src/main/java/a5236/android_game/TitleFragment.java
@@ -1,10 +1,12 @@
 package a5236.android_game;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 
 /**
  * Created by cliffy on 2/20/18.
@@ -12,9 +14,72 @@ import android.view.ViewGroup;
 
 public class TitleFragment extends Fragment {
 
+    private static final String TAG = "TitleFragment";
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_title, container, false);
         /*stuff*/
+        Log.d(TAG, "onCreateView(LayoutInflater, ViewGroup, Bundle) called");
         return v;
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        Log.d(TAG, "onAttach(Context) called");
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated(Bundle) called");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView() called");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach() called");
     }
 }
