3652dda21c83fc58b20696302857655e543f11e5
Austin Payne <payneaustin5@gmail.com>
2018-04-19 11:10:42 -0400

    Add game logic for sensor minigame

--- app/src/main/java/a5236/android_game/multiplayer/minigames/SensorMinigame.java
+++ app/src/main/java/a5236/android_game/multiplayer/minigames/SensorMinigame.java
@@ -1,14 +1,13 @@
 package a5236.android_game.multiplayer.minigames;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import a5236.android_game.Player;
 import a5236.android_game.multiplayer.GameHost;
-
-/**
- * Created by Jared on 4/9/2018.
- */
 
 public class SensorMinigame implements Minigame {
 
@@ -16,7 +15,9 @@ public class SensorMinigame implements Minigame {
 
     private int points;
 
+    private boolean started = false;
     private Set<Player> remainingPlayers;
+    private Map<Player, Long> playerTimes;
 
     public SensorMinigame(GameHost host, int points) {
         this.host = host;
@@ -24,12 +25,7 @@ public class SensorMinigame implements Minigame {
 
         remainingPlayers = new HashSet<>();
         remainingPlayers.addAll(host.players);
-    }
-
-    //TODO
-    @Override
-    public void gameTick() {
-
+        playerTimes = new HashMap<>();
     }
 
     public SensorMinigame(GameHost host) {
@@ -42,8 +38,40 @@ public class SensorMinigame implements Minigame {
     }
 
     @Override
+    public void gameTick() {
+        if (!started) {
+            host.sendToPlayers(host.buildSensorMinigamePacket());
+            started = true;
+        }
+    }
+
+    @Override
     public boolean isFinished() {
         return remainingPlayers.isEmpty();
     }
 
+    public void receiveTime(String participantId, long time) {
+        Player player = null;
+        for (Player remaining : remainingPlayers) {
+            if (remaining.getParticipantId().equals(participantId)) {
+                player = remaining;
+                break;
+            }
+        }
+        if (player != null) {
+            playerTimes.put(player, time);
+            remainingPlayers.remove(player);
+
+            if (isFinished()) {
+                long shortestTime = Collections.min(playerTimes.values());
+                for (Map.Entry<Player, Long> entry : playerTimes.entrySet()) {
+                    if (entry.getValue() <= shortestTime) {
+                        Player winner = entry.getKey();
+                        winner.setPoints(winner.getPoints() + points);
+                        winner.setRounds_won(winner.getRounds_won() + 1);
+                    }
+                }
+            }
+        }
+    }
 }
